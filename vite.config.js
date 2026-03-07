4ba8346607fd3aa5a6d58a921782d4e783483081
ceparker <cparker@audaption.com>
2026-03-07 02:39:24 -0500

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-// Root custom domain deployment (aboutchad.com) uses base "/"
-export default defineConfig({
-  plugins: [react()],
-  base: "/",
-});
