2ac875c26ee0b72c8668d27dfe6a37d928c591a0
ceparker <cparker@audaption.com>
2026-05-16 14:33:08 -0400

    Add combined company landing page registry

---
+++ src/data/allCompanyLandingPages.ts
@@ -0,0 +1,7 @@
+import { companyLandingPages } from "./companyLandingPages";
+import { goFractionalCompanyLandingPages } from "./goFractionalCompanyLandingPages";
+
+export const allCompanyLandingPages = {
+  ...companyLandingPages,
+  ...goFractionalCompanyLandingPages,
+};
