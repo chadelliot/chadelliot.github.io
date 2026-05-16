09ddaaf30823f6c012b0a03dd97ab735376badc0
ceparker <cparker@audaption.com>
2026-05-16 14:47:24 -0400

    Add Wave 2 proposals to shared registry

--- src/data/allCompanyLandingPages.ts
+++ src/data/allCompanyLandingPages.ts
@@ -1,7 +1,9 @@
 import { companyLandingPages } from "./companyLandingPages";
 import { goFractionalCompanyLandingPages } from "./goFractionalCompanyLandingPages";
+import { goFractionalWave2CompanyLandingPages } from "./goFractionalWave2CompanyLandingPages";
 
 export const allCompanyLandingPages = {
   ...companyLandingPages,
   ...goFractionalCompanyLandingPages,
+  ...goFractionalWave2CompanyLandingPages,
 };
