b57b2a4302654ee06d93c0408cd566220fd1a7e8
ceparker <cparker@audaption.com>
2026-05-18 15:28:35 -0400

    Import proposal role source links

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,13 @@ import "./index.css";
 import "./proposal-directory-floating-menu.css";
 import "./proposal-directory-extra-contacts.css";
 import "./proposal-directory-contact-controls.css";
+import "./proposal-directory-role-links.css";
 import "./proposal-directory-floating-menu";
 import "./proposal-directory-fit-rank";
 import "./proposal-directory-extra-contacts";
 import "./proposal-directory-contact-controls";
 import "./proposal-directory-draft-links";
+import "./proposal-directory-role-links";
 import "./proposal-page-phase-timelines";
 
 createRoot(document.getElementById("root")!).render(<App />);
