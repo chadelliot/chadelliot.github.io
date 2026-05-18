3350d1b2a5b67c47f232601e6bfc883fe29f468b
ceparker <cparker@audaption.com>
2026-05-17 21:50:19 -0400

    Import proposal fit rank badges

--- src/main.tsx
+++ src/main.tsx
@@ -3,5 +3,6 @@ import App from "./App.tsx";
 import "./index.css";
 import "./proposal-directory-floating-menu.css";
 import "./proposal-directory-floating-menu";
+import "./proposal-directory-fit-rank";
 
 createRoot(document.getElementById("root")!).render(<App />);
