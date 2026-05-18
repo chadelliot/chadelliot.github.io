bd201db849f8787e5fcfd779aa16e261dea36384
ceparker <cparker@audaption.com>
2026-05-17 23:55:11 -0400

    Import proposal phase timeline normalization

--- src/main.tsx
+++ src/main.tsx
@@ -5,5 +5,6 @@ import "./proposal-directory-floating-menu.css";
 import "./proposal-directory-floating-menu";
 import "./proposal-directory-fit-rank";
 import "./proposal-directory-extra-contacts";
+import "./proposal-page-phase-timelines";
 
 createRoot(document.getElementById("root")!).render(<App />);
