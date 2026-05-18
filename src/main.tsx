414fb5204ef195952230bff82ecfaf8ab266e26e
ceparker <cparker@audaption.com>
2026-05-18 11:41:11 -0400

    Import extra proposal contact styles

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import "./proposal-directory-floating-menu.css";
+import "./proposal-directory-extra-contacts.css";
 import "./proposal-directory-floating-menu";
 import "./proposal-directory-fit-rank";
 import "./proposal-directory-extra-contacts";
