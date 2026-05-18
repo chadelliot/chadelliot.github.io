ce9ce6e8d9cb88720d6e20956fa2b9fda7fecf27
ceparker <cparker@audaption.com>
2026-05-18 12:16:15 -0400

    Import proposal directory contact controls

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,11 @@ import App from "./App.tsx";
 import "./index.css";
 import "./proposal-directory-floating-menu.css";
 import "./proposal-directory-extra-contacts.css";
+import "./proposal-directory-contact-controls.css";
 import "./proposal-directory-floating-menu";
 import "./proposal-directory-fit-rank";
 import "./proposal-directory-extra-contacts";
+import "./proposal-directory-contact-controls";
 import "./proposal-page-phase-timelines";
 
 createRoot(document.getElementById("root")!).render(<App />);
