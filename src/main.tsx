34ec6730e7d817450c9adb656069ede024259e8d
ceparker <cparker@audaption.com>
2026-05-18 17:00:04 -0400

    Import proposal delete controls

--- src/main.tsx
+++ src/main.tsx
@@ -5,12 +5,14 @@ import "./proposal-directory-floating-menu.css";
 import "./proposal-directory-extra-contacts.css";
 import "./proposal-directory-contact-controls.css";
 import "./proposal-directory-role-links.css";
+import "./proposal-directory-delete-controls.css";
 import "./proposal-directory-floating-menu";
 import "./proposal-directory-fit-rank";
 import "./proposal-directory-extra-contacts";
 import "./proposal-directory-contact-controls";
 import "./proposal-directory-draft-links";
 import "./proposal-directory-role-links";
+import "./proposal-directory-delete-controls";
 import "./proposal-page-phase-timelines";
 
 createRoot(document.getElementById("root")!).render(<App />);
