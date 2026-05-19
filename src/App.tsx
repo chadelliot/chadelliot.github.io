8aff85a78cf0bdcaf7e133c8975b2217314f5178
ceparker <cparker@audaption.com>
2026-05-19 12:01:14 -0400

    Route proposal directory to clean V7 template

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,12 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import "./proposal-directory-overrides.css";
 import Index from "./pages/Index";
 import ApproachPage from "./pages/ApproachPage";
 import CareerPage from "./pages/CareerPage";
 import SkillsPage from "./pages/SkillsPage";
 import ContactPage from "./pages/ContactPage";
-import CompanyDirectoryPageV6 from "./pages/CompanyDirectoryPageV6";
+import CompanyDirectoryPageV7 from "./pages/CompanyDirectoryPageV7";
 import CompanyLandingPage from "./pages/CompanyLandingPage";
 import NotFound from "./pages/NotFound";
 import ContactSlideout from "./components/ContactSlideout";
@@ -31,7 +30,7 @@ const App = () => (
           <Route path="/career" element={<CareerPage />} />
           <Route path="/skills" element={<SkillsPage />} />
           <Route path="/contact" element={<ContactPage />} />
-          <Route path="/company" element={<CompanyDirectoryPageV6 />} />
+          <Route path="/company" element={<CompanyDirectoryPageV7 />} />
           <Route path="/company/:slug" element={<CompanyLandingPage />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
